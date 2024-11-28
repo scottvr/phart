2da79647af89c64858542744aa58655048bd81ee
ScottVR <scottvr@gmail.com>
2024-11-28 01:38:27 -0600

    refactor LayoutOptions

--- src/phart/__init__.py
+++ src/phart/__init__.py
@@ -1,5 +1,6 @@
 from .renderer import ASCIIRenderer
-from .styles import NodeStyle, LayoutOptions
+from .styles import NodeStyle
+from .layout import LayoutOptions
 
 __version__ = "0.1.5"
 __all__ = ["ASCIIRenderer", "NodeStyle", "LayoutOptions"]
