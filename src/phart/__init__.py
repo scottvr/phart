f374df21aeb3b021856bbf679b747e820b9e1495
ScottVR <scottvr@gmail.com>
2024-11-30 00:32:42 -0600

    v1.0 - Ready for PyPi

--- src/phart/__init__.py
+++ src/phart/__init__.py
@@ -1,5 +1,5 @@
 from .renderer import ASCIIRenderer
 from .styles import LayoutOptions, NodeStyle
 
-__version__ = "0.1.7"
+__version__ = "1.0.0"
 __all__ = ["ASCIIRenderer", "NodeStyle", "LayoutOptions"]
