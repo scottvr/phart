f41dfb743127d72c0dfde63f726b6706b1fdab1a
ScottVR <scottvr@gmail.com>
2024-12-12 03:01:40 -0600

    bump to 1.1.0

--- src/phart/__init__.py
+++ src/phart/__init__.py
@@ -1,5 +1,5 @@
 from .renderer import ASCIIRenderer
 from .styles import LayoutOptions, NodeStyle
 
-__version__ = "1.0.5"
+__version__ = "1.1.0"
 __all__ = ["ASCIIRenderer", "NodeStyle", "LayoutOptions"]
