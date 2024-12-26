44784cf9870a6c0be8953fd748309074319109db
ScottVR <scottvr@gmail.com>
2024-12-26 15:51:53 -0600

    some bug fixes

--- src/phart/__init__.py
+++ src/phart/__init__.py
@@ -1,5 +1,5 @@
 from .renderer import ASCIIRenderer
 from .styles import LayoutOptions, NodeStyle
 
-__version__ = "1.1.0"
+__version__ = "1.1.2"
 __all__ = ["ASCIIRenderer", "NodeStyle", "LayoutOptions"]
