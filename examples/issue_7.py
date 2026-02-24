6658764aba016aaad1d504d30884d818aef92468
ScottVR <scottvr@gmail.com>
2026-02-24 01:27:33 -0600

    issue #7 test

---
+++ examples/issue_7.py
@@ -0,0 +1,14 @@
+import networkx as nx
+from phart import ASCIIRenderer
+
+g = nx.DiGraph()
+
+g.add_edge("Alice", "Bob", relationship="friend")  
+g.add_edge("Bob", "Alice", relationship="friend")  
+g.add_edge("Bob", "Charlie", relationship="friend")  
+g.add_edge("Charlie", "Bob", relationship="enemy")  
+
+#print(g.edges)
+
+renderer = ASCIIRenderer(g)
+print(renderer.render())
