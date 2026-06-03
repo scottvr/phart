d72f713d1be0ca1eb2d0e3f6528812af1afaa5ee
ScottVR <scottvr@gmail.com>
2026-06-02 19:39:40 -0500

    workflow. bump 2.0.1

--- src/phart/__init__.py
+++ src/phart/__init__.py
@@ -2,7 +2,7 @@ from .layout import CrossPartitionEdge, PartitionPlan
 from .renderer import ASCIIRenderer
 from .styles import LayoutOptions, NodeStyle
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __all__ = [
     "ASCIIRenderer",
     "NodeStyle",
